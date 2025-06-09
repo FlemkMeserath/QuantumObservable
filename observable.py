6433cc36725bab5d454871353426434d8b9f70ab
Francesco Belli <60217731+FlemkMeserath@users.noreply.github.com>
2025-06-09 19:54:14 -0400

    Update observable.py
    Improved Output format

--- observable.py
+++ observable.py
@@ -42,18 +42,26 @@ def read_ICOHP(filename,TRESHOLD,DIST_TRESHOLD):
 
     for i in range(1,len(lines)):
         temp_values = [float(lines[i].split()[3]),  float(lines[i].split()[7]),lines[i].split()[1],lines[i].split()[2]]
-        if abs(temp_values[1]) > TRESHOLD: # and len(ICOHP_values_list) == 0
+        if abs(temp_values[1]) > TRESHOLD: 
             if DIST_TRESHOLD >= temp_values[0]:
                 ICOHP_values_list.append([float(lines[i].split()[3]),  float(lines[i].split()[7]),lines[i].split()[1],lines[i].split()[2]])
-#        NEW_ICOHP_FLAG = True
-#        for j in range(0,len(ICOHP_values_list)):
-#            if temp_values[0] == ICOHP_values_list[j][0] or abs(temp_values[1]) < TRESHOLD:
-#                NEW_ICOHP_FLAG = False
-#        if NEW_ICOHP_FLAG == True and len(ICOHP_values_list) > 0: 
-#            if DIST_TRESHOLD >= temp_values[0]:
-#                ICOHP_values_list.append([float(lines[i].split()[3]), - float(lines[i].split()[7])])
+
     return ICOHP_values_list
 
+
+
+
+
+
+if len(sys.argv) <4:
+    print("Ths code requires the following inputs in order: [POSCAR] [iCOBI_lobster_file] [iCOBI_cutoff (0.018 recommended)] [Interatomic Distance Cutoff]")
+    exit()
+
+
+input_poscar  = sys.argv[1]
+input_ICOHP   = sys.argv[2]
+ICOHP_CUT     = float(sys.argv[3])
+DISTANCE_CUT  = float(sys.argv[4])
 
 
 cell = []
@@ -63,17 +71,13 @@ numbers = []
 types =[]
 
 
-input_poscar  = sys.argv[1]
-input_ICOHP   = sys.argv[2]
-ICOHP_CUT     = float(sys.argv[3])
-DISTANCE_CUT  = float(sys.argv[4])
 
 cell, atoms, tmp_num, types, input_type = read_poscar(input_poscar)
 variab =                      read_ICOHP(input_ICOHP,ICOHP_CUT,DISTANCE_CUT)
 
-print("variab",variab)
 
-print(variab)
+
+
 
 
 for i in range(0,len(atoms)):
@@ -93,16 +97,19 @@ for i in range(0,len(tmp_num)):
     for j in range(0,tmp_num[i]):
         numbers.append(count)
     
-print(numbers,types)
+
 
 
 cell_SPG_LIB = (cell,atoms,numbers)
-equivalent_atoms = spg.get_symmetry(cell_SPG_LIB, symprec=5e-03, angle_tolerance=-1.0, mag_symprec=-1.0, is_magnetic=True)['equivalent_atoms']
-print(equivalent_atoms)
+equivalent_atoms = spg.get_symmetry(cell_SPG_LIB, symprec=5e-02, angle_tolerance=-1.0, mag_symprec=-1.0, is_magnetic=True)['equivalent_atoms']
 
-#print(pos,atoms,cell)
+
+
 data = []
+stored_int =  []
 count = 0
+
+print("A1  Id1 A2  Id2 Dist    iCOBI   D_iCOBI  Repeated Cell")
 for i in range(0,len(pos)):
     count = 0
     data.append([0,0,0])#,0,0,0])
@@ -118,48 +125,45 @@ for i in range(0,len(pos)):
                         if ddd > variab[q][0] - 0.0001 and ddd < variab[q][0] + 0.0001:
                             if variab[q][2] == str(types[numbers[i]-1]) + str(i+1) or variab[q][3] == str(types[numbers[i]-1]) + str(i+1):
                                 if variab[q][2] == str(types[numbers[j]-1]) + str(j+1) or variab[q][3] == str(types[numbers[j]-1]) + str(j+1):
-                                    count = count + 1 #*variab[q][1]
-                                    print(types[numbers[i]-1],i+1,types[numbers[j]-1],j+1,variab[q][0],variab[q][1],ddd,pos[j][0],pos[j][1],pos[j][2],a,b,c)
+                                    count = count + 1 
+                                    print( f"{types[numbers[i]-1]:<3} " f"{i+1:<3} " f"{types[numbers[j]-1]:<3} " f"{j+1:<3} "     f"{ddd:<2.5f} " f"{variab[q][0]:<2.5f} " f"{variab[q][1]:<2.6f} " f"{a:<1} " f"{b:<1} " f"{c:<1}"
+)
                                     data[i][0] = data[i][0] - distance[0]/ddd*variab[q][1] 
                                     data[i][1] = data[i][1] - distance[1]/ddd*variab[q][1] 
                                     data[i][2] = data[i][2] - distance[2]/ddd*variab[q][1]
-#                                    data[i][3] = data[i][3] - distance[0]
-#                                    data[i][4] = data[i][4] - distance[1]
-#                                    data[i][5] = data[i][5] - distance[2]
-    print(i,count)
+
+    stored_int.append([i,count])
     if count !=0 :
         data[i][0] = data[i][0]/count
         data[i][1] = data[i][1]/count
         data[i][2] = data[i][2]/count
-#        data[i][3] = data[i][3]/count
-#        data[i][4] = data[i][4]/count
-#        data[i][5] = data[i][5]/count
+
     else:
         data[i][0] = 0
         data[i][1] = 0
         data[i][2] = 0
-#        data[i][3] = 0
-#        data[i][4] = 0
-#        data[i][5] = 0
 
-for i in data: print(i)
+print("")
+print("---------Interactions---------")
+print("")
+for i in stored_int: print('Atom: ' + f"{types[numbers[i[0]]-1]:<3}" +  f"({i[0]:<3}) " + '  Interactions: ' f"({i[1]:<3})" +   '  Equivalent to ' + str(equivalent_atoms[i[0]]) )
+print("")
+print("---------Symmetry Indexes---------")
+print("")
+
 
 count = 0
-eq_count = equivalent_atoms[0]
 S_VAR = 0
-#S_VAR2 = 0
+
+
 for i in range(0,len(equivalent_atoms)):
-    if eq_count == equivalent_atoms[i]:
-        S_VAR = S_VAR + math.sqrt(data[i][0]*data[i][0]+ data[i][1]*data[i][1] + data[i][2]*data[i][2])
-#        S_VAR2 = S_VAR2 + math.sqrt(data[i][3]*data[i][3]+ data[i][4]*data[i][4] + data[i][5]*data[i][5])
-        eq_count = equivalent_atoms[i]
-        count = count +1
-    else:
-        print(S_VAR/count,count)
-#        print(S_VAR2/count,count,"d")
-        count = 1
-        eq_count = equivalent_atoms[i]
-        S_VAR = math.sqrt(data[i][0]*data[i][0]+ data[i][1]*data[i][1] + data[i][2]*data[i][2])
-#        S_VAR2 = math.sqrt(data[i][3]*data[i][3]+ data[i][4]*data[i][4] + data[i][5]*data[i][5])
-print(S_VAR/count,count)
-#print(S_VAR2/count,count,"d")
+    tmp_atm = equivalent_atoms[i]
+    for j in range(0,len(equivalent_atoms)):
+
+        if tmp_atm != -1 and  tmp_atm == equivalent_atoms[j]:
+            S_VAR = S_VAR + math.sqrt(data[j][0]*data[j][0]+ data[j][1]*data[j][1] + data[j][2]*data[j][2])
+            equivalent_atoms[j] = -1
+            count = count +1
+    if tmp_atm != -1:    print("Atom [Multiplicity]: " + f"{types[numbers[i]-1]:<2}" + " [" + str(count) + "] " + " - SI: " + f"{S_VAR/count:<1.5f}")
+    count = 0
+    S_VAR = 0
